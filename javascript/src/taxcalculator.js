0efa880f9efa122b5cf3ca85a0a5a92913c08029
Pascal <pascal@macbook.local>
2018-05-07 16:49:30 +0200

    add getGrandtotal/0

--- javascript/src/taxcalculator.js
+++ javascript/src/taxcalculator.js
@@ -1,12 +1,31 @@
 
 export class TaxCalculator {
     constructor(database, shoppingCart) {
+
+
         this.database = database;
         this.shoppingCart = shoppingCart;
+
+
     }
 
     getGrandtotal() {
-        return 0; // TODO
+      let grandtotal = 0;
+
+      // for (let shoppingCartItem of this.shoppingCart) {
+      //   let productItem = this.database.getProduct(shoppingCartItem.productId);
+      //   grandtotal += productItem.price * shoppingCartItem.quantity;
+      // }
+      this.shoppingCart.forEach(shoppingCartItem => {
+          let productItem = this.database.getProduct(shoppingCartItem.productId);
+          let priceFloat = parseFloat(productItem.price);
+          let quantityFloat = parseFloat(shoppingCartItem.quantity);
+          grandtotal += priceFloat * quantityFloat;
+          console.log(Number.isNaN(priceFloat), Number.isNaN(quantityFloat), Number.isNaN(grandtotal));
+
+      });
+      console.log(grandtotal);
+      return parseFloat(grandtotal);
     }
 
     getGrandtotalTax() {
