933ad82b981be0d2c5731181a64c923d9ffef8a8
Pascal <pascal@macbook.local>
2018-05-08 10:57:01 +0200

    helper function update and fixes +fixed roundings +add helper functions and tests for those +completed test check if all shopping cart items are in db

--- javascript/src/taxcalculator.js
+++ javascript/src/taxcalculator.js
@@ -6,47 +6,58 @@ export class TaxCalculator {
         this.database = database;
         this.shoppingCart = shoppingCart;
 
+    }
 
+    round(number, precision) {
+      var shift = function (number, precision) {
+        var numArray = ("" + number).split("e");
+        return  + (numArray[0] + "e" + (numArray[1] ? (+numArray[1] + precision) : precision));
+      };
+      return shift(Math.round(shift(number, +precision)), -precision);
     }
 
     getGrandtotal() {
       let grandtotal = 0.00;
       this.shoppingCart.forEach(shoppingCartItem => {
-          let productItem = this.database.getProduct(shoppingCartItem.productId);
-          grandtotal += productItem.price * shoppingCartItem.quantity;
+        let productItem = this.database.getProduct(shoppingCartItem.productId);
+        grandtotal += productItem.price * shoppingCartItem.quantity;
       });
-      return grandtotal;
+      return this.round(grandtotal,2);
     }
 
     getGrandtotalTax() {
       let grandtotalTax = 0.00;
       this.shoppingCart.forEach(shoppingCartItem => {
-          let productItem = this.database.getProduct(shoppingCartItem.productId);
-          grandtotalTax += Math.round((productItem.price * shoppingCartItem.quantity) * productItem.taxRate, 2);
+        let productItem = this.database.getProduct(shoppingCartItem.productId);
+        grandtotalTax += this.getItemTax(productItem) * shoppingCartItem.quantity;
       });
-      return grandtotalTax;
+      return this.round(grandtotalTax,2);
     }
 
     getSubtotal(taxRate) {
       let subtotal = 0.00;
       this.shoppingCart.forEach(shoppingCartItem => {
-          let productItem = this.database.getProduct(shoppingCartItem.productId);
-          if (productItem.taxRate == taxRate) {
-            subtotal += productItem.price * shoppingCartItem.quantity;
-          }
+        let productItem = this.database.getProduct(shoppingCartItem.productId);
+        if (productItem.taxRate == taxRate) {
+          subtotal += productItem.price * shoppingCartItem.quantity;
+        }
       });
-      return subtotal;
+      return this.round(subtotal,2);
     }
 
     getSubtotalTax(taxRate) {
       let subtotalTax = 0;
       this.shoppingCart.forEach(shoppingCartItem => {
-          let productItem = this.database.getProduct(shoppingCartItem.productId);
-          if (productItem.taxRate == taxRate) {
-            subtotalTax += productItem.price * shoppingCartItem.quantity;
-          }
+        let productItem = this.database.getProduct(shoppingCartItem.productId);
+        if (productItem.taxRate == taxRate) {
+          subtotalTax += this.getItemTax(productItem) * shoppingCartItem.quantity;
+        }
       });
-      return subtotalTax - (subtotalTax / (taxRate+1));
+      return this.round(subtotalTax,2);
+    }
+
+    getItemTax(productItem) {
+      return productItem.price - productItem.price / (productItem.taxRate+1)
     }
 
 }
--- javascript/src/taxcalculator.test.js
+++ javascript/src/taxcalculator.test.js
@@ -12,42 +12,56 @@ let shoppingCart = [
 test('have all products of shopping cart in database', () => {
     let db = new Database();
     shoppingCart.forEach(item => {
-        expect(db.getProduct(item.productId)).toBeDefined();
+        expect(db.getProduct(item.productId).productId).toBe(item.productId);
     });
+});
+
+//HELPER TESTS//
+test('round/2 is supposed to round up when arg1>x.xx5 and down when arg1<x.xx4 if the precision is 2', () => {
+  let db = new Database();
+  let calc = new TaxCalculator(db, shoppingCart);
+  expect(calc.round(6.664534,2)).toBe(6.66);
+  expect(calc.round(4.035534,2)).toBe(4.04);
+});
+test('getItemTax/1 is supposed to be netto price of red wine', () => {
+  let db = new Database();
+  let calc = new TaxCalculator(db, shoppingCart);
+  let productItem = db.getProduct("red wine")
+  expect(calc.getItemTax(productItem)).toBe(productItem.price - productItem.price / (productItem.taxRate+1));
 });
 
 //GRANDTOTAL TESTS//
 test('getGrandtotal/0 is supposed to be 33.55', () => {
   let db = new Database();
   let calc = new TaxCalculator(db, shoppingCart);
-  expect(calc.getGrandtotal()).toBeCloseTo(33.55);
+  expect(calc.getGrandtotal()).toBe(33.55);
 });
 
 test('getGrandtotalTax/0 is supposed to be 4', () => {
   let db = new Database();
   let calc = new TaxCalculator(db, shoppingCart);
-  expect(calc.getGrandtotalTax()).toBeCloseTo(4);
+  expect(calc.getGrandtotalTax()).toBe(4);
 });
 
 //SUBTOTAL TESTS//
 test('getSubtotal/1 is supposed to be 9.98 with a tax rate of 7%', () => {
   let db = new Database();
   let calc = new TaxCalculator(db, shoppingCart);
-  expect(calc.getSubtotal(0.07)).toBeCloseTo(9.98);
+  expect(calc.getSubtotal(0.07)).toBe(9.98);
 });
 test('getSubtotalTax/1 is supposed to be 0.65 with a tax rate of 7%', () => {
   let db = new Database();
   let calc = new TaxCalculator(db, shoppingCart);
-  expect(calc.getSubtotalTax(0.07)).toBeCloseTo(0.65);
+  expect(calc.getSubtotalTax(0.07)).toBe(0.65);
 });
 
 test('getSubtotal/1 is supposed to be 20.97 with a tax rate of 19%', () => {
   let db = new Database();
   let calc = new TaxCalculator(db, shoppingCart);
-  expect(calc.getSubtotal(0.19)).toBeCloseTo(20.97);
+  expect(calc.getSubtotal(0.19)).toBe(20.97);
 });
 test('getSubtotalTax/1 is supposed to be 3.35 with a tax rate of 19%', () => {
   let db = new Database();
   let calc = new TaxCalculator(db, shoppingCart);
-  expect(calc.getSubtotalTax(0.19)).toBeCloseTo(3.35);
+  expect(calc.getSubtotalTax(0.19)).toBe(3.35);
 });
